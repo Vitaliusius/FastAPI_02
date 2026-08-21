af55fde1d61f22620c43cd43466cf84980bacc0d
Vitaliy <vitalianobro@gmail.com>
2026-08-21 16:08:19 +0300

    add API endpoint GET /users/me

--- src/main.py
+++ src/main.py
@@ -1,15 +1,19 @@
-from pathlib import Path
-
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 
-@app.get("/")
-def get_index():
-    return FileResponse(Path("frontend") / "index.html")
+@app.get("/frontend-api/users/me")
+def get_user_me():
+    return {
+        "email": "user@example.com",
+        "isActive": True,
+        "profileId": "1",
+        "registeredAt": "2025-06-15T18:29:56+00:00",
+        "updatedAt": "2025-06-15T18:29:56+00:00",
+        "username": "superuser",
+    }
 
 
-app.mount("/", StaticFiles(directory="frontend"), name="frontend")
+app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
